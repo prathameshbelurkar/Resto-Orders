f5ac6bd7a0fa9f92ff4ecc4f8996fde2b3495ea1
Prathamesh Belurkar <76102350+prathameshbelurkar@users.noreply.github.com>
2023-10-14 00:19:04 +0530

    added recipe model, recipe-list view

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
-  styleUrls: ['./recipe-list.component.css']
+  styleUrls: ['./recipe-list.component.css'],
 })
 export class RecipeListComponent {
+  recipes: Recipe[] = [
+    new Recipe(
+      'Test Recipe',
+      'The Amazing Recipe',
+      'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&webp=true&resize=375,341'
+    ),
+  ];
 
+  constructor() {}
 }
