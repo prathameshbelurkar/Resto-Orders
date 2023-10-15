ed5f931da63887be3b78816926217400b6d093a4
Prathamesh Belurkar <76102350+prathameshbelurkar@users.noreply.github.com>
2023-10-15 12:26:47 +0530

    styling recipe components

--- src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -3,8 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-recipe-detail',
   templateUrl: './recipe-detail.component.html',
-  styleUrls: ['./recipe-detail.component.css']
+  styleUrls: ['./recipe-detail.component.css'],
 })
-export class RecipeDetailComponent {
-
-}
+export class RecipeDetailComponent {}
